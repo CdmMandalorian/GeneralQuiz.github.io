12c0d57e10c72730077a41b4f56e75ef31f753bc
Armando Galindo <egalindounited@gmail.com>
2020-08-31 22:43:42 -0700

    modified all files

--- script.js
+++ script.js
@@ -1,114 +1,353 @@
 var questionsArray = [
-  {
-      questionText: "Which one is not a Property to adjust positioning on html elements?",
-      quizOptions: {
-          A: "Float",
-          B: "R-Index",
-          C: "Position",
-          D: "Clear"
-      },
-      questionAnswer: "B"
-  },
-  {
-    questionText: "Which of the following methods are not acceptable to Specified Colors in Css?",
-    quizOptions: {
-        A: "Hexadecimal Colors ",
-        B: "RGB/RGBA Colors",
-        C: "Corsiar Colors",
-        D: "using the Color keyword"
+    {
+        question: "Which one is not a Property to adjust positioning on html elements?",
+        
+        answers: [
+            
+            {
+                
+                id: 1,
+                
+                answer: "Float",
+                
+                correct: false,
+           
+            },
+           
+            {
+                
+                id: 2,
+                
+                answer: "P-Index",
+                
+                correct: true,
+            
+            },
+            
+            {
+               
+                id: 3,
+                
+                answer: "Clear",
+                
+                correct: false,
+            
+            },
+            
+            {
+               
+                id: 4,
+                
+                answer: "Display",
+                
+                correct: false,
+           
+            },
+       
+        ],
+    
     },
-    questionAnswer: "C"
-  },
-  {
-    questionText: "Which Value belongs with the Position property?",
-    quizOptions: {
-        A: "Shock",
-        B: "Pothole",
-        C: "Introvert",
-        D: "Static"
+    
+    {
+       
+        question: "How do you select a single element regardless of their ID or Class",
+       
+        answers: [
+            
+            {
+                
+                id: 1,
+                
+                answer: "querySelect",
+                
+                correct: false,
+          
+            },
+           
+            {
+                
+                id: 2,
+               
+                answer: "querySelectorAll",
+                
+                correct: false,
+          
+            },
+           
+            {
+                
+                id: 3,
+               
+                answer: "querySelector",
+               
+                correct: true,
+           
+            },
+           
+            {
+                
+                id: 4,
+               
+                answer: "addSelectorbtn",
+                
+                correct: false,
+           
+            },
+       
+        ],
+   
     },
-    questionAnswer: "D"
-  },
-  {
-    questionText: "What are the 3 Values for Text-align?",
-    quizOptions: {
-        A: "Left, Right, UP",
-        B: "Up, Center, Down",
-        C: "Hover, Middle, Under",
-        D: "Left, Right, Center"
+   
+    {
+        
+        question: "Which Value belongs with the Position Property?",
+        
+        answers: [
+            
+            {
+                
+                id: 1,
+                
+                answer: "Static",
+               
+                correct: true,
+           
+            },
+            
+            {
+                
+                id: 2,
+                
+                answer: "Shock",
+                
+                correct: false,
+            
+            },
+            
+            {
+                
+                id: 3,
+                
+                answer: "Introvert",
+                
+                correct: false,
+            
+            },
+            
+            {
+               
+                id: 4,
+                
+                answer: "Retrovert",
+                
+                correct: false,
+           
+            },
+        
+        ],
+   
     },
-    questionAnswer: "D"
-  },
-  {
-    questionText: "Which tag would produce the biggest Headline in HTML?",
-    quizOptions: {
-        A: "//RIP JAVA!",
-        B: "<h1>",
-        C: "<h3>",
-        D: "<p>"
+    
+    {
+        
+        question: "What does DOM stand for?",
+        
+        answers: [
+           
+            {
+               
+                id: 1,
+                
+                answer: "Document Object Model",
+                
+                correct: true,
+           
+            },
+            
+            {
+                
+                id: 2,
+                
+                answer: "Development Overall Maximuss",
+                
+                correct: false,
+            
+            },
+           
+            {
+               
+                id: 3,
+                
+                answer: "Deep Occultic Music ",
+                
+                correct: false,
+            
+            },
+            
+            {
+                
+                id: 4,
+                
+                answer: "Direct Order Media",
+               
+                correct: false,
+            
+            },
+        
+        ],
+    
     },
-    questionAnswer: "B"
-},];
+    
+    {
+        
+        question: "With wich array method, can you remove the last element",
+       
+        answers: [
+            
+            {
+               
+                id: 1,
+                
+                answer: "Snack",
+                
+                correct: false,
+           
+            },
+           
+            {
+                
+                id: 2,
+                
+                answer: "Snap",
+               
+                correct: false,
+           
+            },
+           
+            {
+               
+                id: 3,
+               
+                answer: "Crackle",
+                
+                correct: false,
+           
+            },
+            
+            {
+               
+                id: 4,
+                
+                answer: "Pop",
+                
+                correct: true,
+            
+            },
+        
+        ],
+    
+    },
 
-var quizOptions ={
+],    
+
+var quizOptions = {
+   
     totalScore: 0,
+    
     questionIndex: 0,
+   
     correctCount: 0,
+
 };
 
-var questionText = document.querySelector("#questions-text");
-var questionsContiner = document.querySelector("#questions")
+var questionTextOutput = document.querySelector("#question-text");
 
+var questionsContainer = document.querySelector("#questions");
 
-//we have a first question
-//when the question is answered it will either be the correct answer, or not.
-//when the answer is correct, the totalScore will go up by 10 and the correctCount go up by 1
-//when the answer is correct, the totalScore will go down by 5 and the correctCount will stay the same
-//whether the question is right or wrong, the questionIndex will go up by one
 function gameOver() {
-    questionsContiner.innerHTML = "";
+
+    questionsContainer.innerHTML = "";
+
     var gameOver
+
     alert(
-        quizOptions.totalScore + "You are " + quizOptions.correctCount + " out of " + questionsArray.length); 
 
-function generateQuestion(){
-    if(quizOptions.questionIndex === questionsArray.length -1) {
-        gameOver();
-        return;
+        quizOptions.totalScore + "You are " + quizOptions.correctCount + " out of " + questionsArray.length)};
+
+    function generateQuestion() {
+        
+        if (quizOptions.questionIndex === questionsArray.length - 1) {
+            
+            gameOver();
+            
+            return;
+        
+        }
+        
+        var question = questionsArray[quizOptions.questionIndex]
+        
+        console.log(question);
+        
+        questionTextOutput.textContent = question.question;
+        
+        for (i = 0; i < question.answers.length; i++) {
+            
+            var questionAnswer = document.createElement("p")
+            
+            questionAnswer.textContent = question.answers[i].answer;
+            
+            questionAnswer.classList.add("green");
+            
+            questionAnswer.setAttribute("data-value", quastion.answers[1].correct);
+            
+            questionsContainer.append(questionAnswer);
+        
+        }
+    
     }
 
-    var question = questionsArray[quizOptions.questionIndex]
-    questionText.textContent = question.question;
-    for(i=0;i< question.anwers.length; i++){
-        var questionAnswer = document.createElement("p")
-        questionAnswer.textContent = question.anwers[i].answer;
-        question.classList.add("green");
-        questionAnswer.setAttribute("data-value", quastion.anwers[1].correct);
-        questionsContiner.append(questionAnswer);
-    }
-}
-
-
-generateQuestion();
-
-function checkAnswer(answer) {
-    console.log(answer === "true");
-    if (answer) {
-        quizOptions.totalScore = quizOptions.totalScore + 10;
-        quizOptions.correctCount++;
-    }else{
-        quizOptions.totalScore = quizOptions.totalScore - 5;
-    }
-    console.log(quizOptions.totalScore);
-    console.log(quizOptions.correctCount);
-}
-
-questionsContiner.addEventListener("click", function(event) {
-    var correctAnswer = event.target.getAttribute("data-value");
-
-    alert(correctAnswer + " " + quizOptions.totalScore);
-    quizOptions.questionIndex++;
-    questionsContiner.innerHTML = "";
     generateQuestion();
-});
-}
+
+    function checkAnswer(answer) {
+
+        console.log(answer);
+        
+        if (4 === "4") {
+    
+        console.log("What?");
+
+        }
+    
+        if (answer === "true") {
+    
+            quizOptions.totalScore = quizOptions.totalScore + 10;
+    
+            quizOptions.correctCount++;
+    
+        } else {
+    
+            quizOptions.totalScore = quizOptions.totalScore - 5;
+    
+        }
+    
+        console.log(quizOptions.totalScore);
+    
+        console.log(quizOptions.correctCount);
+    
+    }
+
+    questionsContainer.addEventListener("click", function (event) {
+    
+        var correctAnswer = event.target.getAttribute("data-value");
+    
+        checkAnswer(correctAnswer);
+    
+        alert(correctAnswer);
+    
+        quizOptions.questionIndex++;
+    
+        questionsContainer.innerHTML = "";
+    
+        generateQuestion();
+    
+    });
